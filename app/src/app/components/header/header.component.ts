f61693bb3e300594fee6ba79289ec0281e405bc5
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2025-03-31 06:19:03 +0800

    feat(angular): implement home route, add a hero bg with black gradient

--- app/src/app/components/header/header.component.ts
+++ app/src/app/components/header/header.component.ts
@@ -1,12 +1,16 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faHome, faUpload } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   logo = '/images/logo.png';
+  faHome = faHome;
+  faUpload = faUpload;
 }

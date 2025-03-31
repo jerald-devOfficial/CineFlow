bcdd40acab172e48515413157247169c18ec11c5
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2025-03-31 23:50:52 +0800

    feat(angular): implement a 404 not found page

--- app/src/app/not-found/not-found.component.ts
+++ app/src/app/not-found/not-found.component.ts
@@ -1,9 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-not-found-page',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule, FontAwesomeModule],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css',
 })
-export class NotFoundPageComponent {}
+export class NotFoundPageComponent {
+  faHome = faHome;
+
+  constructor(private router: Router) {}
+
+  goHome() {
+    this.router.navigate(['/']);
+  }
+}

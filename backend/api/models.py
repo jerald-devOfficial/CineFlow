85c00411eed9679e70f6c2b3cf27e08daf72e84b
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2025-03-31 20:59:24 +0800

    feat(django): update backend to handle thumbnail upload, fix cors issue

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class MovieUpload(models.Model):
     title = models.CharField(max_length=150)
     video_file = models.FileField(upload_to='movies', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='thumbnails', blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     created_date = models.DateField(auto_now_add=True)
     modified_date = models.DateField(auto_now=True)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -134,8 +135,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS POLICY
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:4200'
-    ]
+    'http://localhost:4200',
+]
+    
 CORS_ALLOW_METHODS = [
     "DELETE",
     "GET",
